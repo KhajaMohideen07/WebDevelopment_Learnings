2ab057368add0bc146ef6d8307768057dd8e0044
KhajaMohideen07 <khajam799@gmail.com>
2023-09-18 22:04:42 +0530

    AdminDashboard Created

---
+++ Admin Dashboard/orders.js
@@ -0,0 +1,32 @@
+const Orders=[
+    {
+        productName:'New Product1',
+        productNumber:'85631',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'New Product2',
+        productNumber:'34353',
+        paymentStatus:'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName:'New Product3',
+        productNumber:'35634',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'New Product4',
+        productNumber:'85631',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    },
+    {
+        productName:'New Product5',
+        productNumber:'89656',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    }
+]
